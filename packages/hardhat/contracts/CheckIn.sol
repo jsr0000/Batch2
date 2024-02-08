7506d7199a4eefce6d828a113f9ea2222295364e
jsr0000 <josh.regnart@gmail.com>
2024-02-08 12:18:19 +0000

    for testing

---
+++ packages/hardhat/contracts/CheckIn.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: MIT
+pragma solidity >=0.8.0 <0.9.0;
+
+
+interface IBatchRegistry {
+    function checkIn() external;
+}
+
+contract CheckIn {
+    function _checkMeIn(address _batchRegistry) external {
+        IBatchRegistry(_batchRegistry).checkIn();
+    } 
+}
